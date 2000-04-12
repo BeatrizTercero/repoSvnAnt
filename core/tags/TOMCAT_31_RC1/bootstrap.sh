71b7b00be5346025db7479fdf3cd08dfde802fb8
(no author) <(no author)@13f79535-47bb-0310-9956-ffa450edef68>
2000-04-12 16:20:53 +0000

    This commit was manufactured by cvs2svn to create tag 'TOMCAT_31_RC1'.
    git-svn-id: http://svn.apache.org/repos/asf/ant@267654 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/tags/TOMCAT_31_RC1/bootstrap.sh
@@ -0,0 +1,30 @@
+if [ -f $HOME/.antrc ] ; then 
+  . $HOME/.antrc
+fi
+
+SRCDIR=src/main/org/apache/tools
+CLASSDIR=classes
+CLASSPATH=${CLASSPATH}:${JAVA_HOME}/lib/classes.zip:${JAVA_HOME}/lib/tools.jar
+CLASSPATH=${CLASSPATH}:lib/xml.jar:src/main:${CLASSDIR}
+
+mkdir -p ${CLASSDIR}
+
+export CLASSPATH
+echo $CLASSPATH
+
+javac  -d ${CLASSDIR} ${SRCDIR}/tar/*.java
+javac  -d ${CLASSDIR} ${SRCDIR}/ant/*.java
+javac  -d ${CLASSDIR} ${SRCDIR}/ant/taskdefs/*.java
+
+cp src/main/org/apache/tools/ant/taskdefs/defaults.properties ${CLASSDIR}/org/apache/tools/ant/taskdefs
+cp src/main/org/apache/tools/ant/parser.properties ${CLASSDIR}/org/apache/tools/ant
+
+java org.apache.tools.ant.Main main
+java org.apache.tools.ant.Main clean 
+
+if test ! -d bin; then mkdir bin; fi
+cp src/bin/antRun bin
+chmod +x bin/antRun
+
+rm -rf ${CLASSDIR}
+
