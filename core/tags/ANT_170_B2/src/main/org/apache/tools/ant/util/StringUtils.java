526a3b9c85ca0776d61e2a34e1c1386032231dae
antoine <antoine@13f79535-47bb-0310-9956-ffa450edef68>
2006-09-17 13:32:48 +0000

    Tagging version 1.7.0Beta2 of Ant
    git-svn-id: http://svn.apache.org/repos/asf/ant@447067 13f79535-47bb-0310-9956-ffa450edef68

---
+++ core/tags/ANT_170_B2/src/main/org/apache/tools/ant/util/StringUtils.java
@@ -0,0 +1,133 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.tools.ant.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Vector;
+
+/**
+ * A set of helper methods related to string manipulation.
+ *
+ */
+public final class StringUtils {
+
+    /**
+     * constructor to stop anyone instantiating the class
+     */
+    private StringUtils() {
+    }
+
+    /** the line separator for this OS */
+    public static final String LINE_SEP = System.getProperty("line.separator");
+
+    /**
+     * Splits up a string into a list of lines. It is equivalent
+     * to <tt>split(data, '\n')</tt>.
+     * @param data the string to split up into lines.
+     * @return the list of lines available in the string.
+     */
+    public static Vector lineSplit(String data) {
+        return split(data, '\n');
+    }
+
+    /**
+     * Splits up a string where elements are separated by a specific
+     * character and return all elements.
+     * @param data the string to split up.
+     * @param ch the separator character.
+     * @return the list of elements.
+     */
+    public static Vector split(String data, int ch) {
+        Vector elems = new Vector();
+        int pos = -1;
+        int i = 0;
+        while ((pos = data.indexOf(ch, i)) != -1) {
+            String elem = data.substring(i, pos);
+            elems.addElement(elem);
+            i = pos + 1;
+        }
+        elems.addElement(data.substring(i));
+        return elems;
+    }
+
+    /**
+     * Replace occurrences into a string.
+     * @param data the string to replace occurrences into
+     * @param from the occurrence to replace.
+     * @param to the occurrence to be used as a replacement.
+     * @return the new string with replaced occurrences.
+     */
+    public static String replace(String data, String from, String to) {
+        StringBuffer buf = new StringBuffer(data.length());
+        int pos = -1;
+        int i = 0;
+        while ((pos = data.indexOf(from, i)) != -1) {
+            buf.append(data.substring(i, pos)).append(to);
+            i = pos + from.length();
+        }
+        buf.append(data.substring(i));
+        return buf.toString();
+    }
+
+    /**
+     * Convenient method to retrieve the full stacktrace from a given exception.
+     * @param t the exception to get the stacktrace from.
+     * @return the stacktrace from the given exception.
+     */
+    public static String getStackTrace(Throwable t) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw, true);
+        t.printStackTrace(pw);
+        pw.flush();
+        pw.close();
+        return sw.toString();
+    }
+
+    /**
+     * Checks that a string buffer ends up with a given string. It may sound trivial with the existing
+     * JDK API but the various implementation among JDKs can make those methods extremely resource intensive
+     * and perform poorly due to massive memory allocation and copying. See
+     * @param buffer the buffer to perform the check on
+     * @param suffix the suffix
+     * @return  <code>true</code> if the character sequence represented by the
+     *          argument is a suffix of the character sequence represented by
+     *          the StringBuffer object; <code>false</code> otherwise. Note that the
+     *          result will be <code>true</code> if the argument is the
+     *          empty string.
+     */
+    public static boolean endsWith(StringBuffer buffer, String suffix) {
+        if (suffix.length() > buffer.length()) {
+            return false;
+        }
+        // this loop is done on purpose to avoid memory allocation performance problems on various JDKs
+        // StringBuffer.lastIndexOf() was introduced in jdk 1.4 and implementation is ok though does allocation/copying
+        // StringBuffer.toString().endsWith() does massive memory allocation/copying on JDK 1.5
+        // See http://issues.apache.org/bugzilla/show_bug.cgi?id=37169
+        int endIndex = suffix.length() - 1;
+        int bufferIndex = buffer.length() - 1;
+        while ( endIndex >= 0 ) {
+            if ( buffer.charAt(bufferIndex) != suffix.charAt(endIndex) ) {
+                return false;
+            }
+            bufferIndex--;
+            endIndex--;
+        }
+        return true;
+    }
+}
