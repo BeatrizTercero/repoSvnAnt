e982ad255b26a286d269d917fbada142c8be6e8f
jhm <jhm@13f79535-47bb-0310-9956-ffa450edef68>
2007-11-14 09:35:30 +0000

    remove TODO comment
    git-svn-id: http://svn.apache.org/repos/asf/ant@594813 13f79535-47bb-0310-9956-ffa450edef68

--- sandbox/mocks/src/org/apache/xml/resolver/CatalogEntry.java
+++ sandbox/mocks/src/org/apache/xml/resolver/CatalogEntry.java
@@ -21,12 +21,10 @@ package org.apache.xml.resolver;
 public class CatalogEntry {
 
     public int getEntryType() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public Object getEntryArg(int i) {
-        // TODO Auto-generated method stub
         return null;
     }
 }
