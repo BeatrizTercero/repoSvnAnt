6f7941c57522fc9a2f51386e6c8d1f578bdf48a7
xavier <xavier@13f79535-47bb-0310-9956-ffa450edef68>
2008-01-03 12:29:04 +0000

    introduce latest-milestone version of the documentation and make the default documentation point to this version instead of trunk version
    git-svn-id: http://svn.apache.org/repos/asf/ant@608463 13f79535-47bb-0310-9956-ffa450edef68

--- ivy/site/config.js
+++ ivy/site/config.js
@@ -2,7 +2,7 @@ xooki.util.mix({debug:true,
 	jira: {ids: ['IVY'], url: 'http://issues.apache.org/jira'}, 
 	shortcuts: {
 		svn: {pre: 'https://svn.apache.org/repos/asf/ant/ivy/core/trunk/'},
-		ant: {pre: xooki.c.relativeRoot+'history/trunk/use/', post:'.html'},
-		doc: {pre: xooki.c.relativeRoot+'history/trunk/', post:'.html'}
+		ant: {pre: xooki.c.relativeRoot+'history/latest-milestone/use/', post:'.html'},
+		doc: {pre: xooki.c.relativeRoot+'history/latest-milestone/', post:'.html'}
 	}
 }, xooki.c, false);
