111328bbc115a54603b8ac7d325f6f586a06984d
bodewig <bodewig@13f79535-47bb-0310-9956-ffa450edef68>
2008-10-31 04:45:37 +0000

    convenience base class
    git-svn-id: http://svn.apache.org/repos/asf/ant@709346 13f79535-47bb-0310-9956-ffa450edef68

---
+++ sandbox/javafront/src/etc/examples/AntUnitTest.java
@@ -0,0 +1,39 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.ant.javafront.example;
+
+import org.apache.ant.javafront.BuildFileBase;
+import org.apache.ant.javafront.annotations.AntProject;
+import org.apache.ant.javafront.annotations.AntTarget;
+import org.apache.tools.ant.Project;
+
+@AntProject(Name="example using AntUnit", BaseDir="../../..",
+            DefaultTarget="describe")
+public class AntUnitTest extends BuildFileBase {
+    public AntUnitTest(Project p) {
+        super(p);
+    }
+
+    @AntTarget(Description="describes this build file")
+    public void describe() {
+        build().newTag("echo")
+            .withAttribute("message", "${ant.version}").execute();
+        build().newTag("echo")
+            .withNestedText("Demonstrates running an AntUnit test.").execute();
+    }
+}
