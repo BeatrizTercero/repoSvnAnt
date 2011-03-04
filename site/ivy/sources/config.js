8b51e22ce5e4633be2c5717af311a507c1206d51
hibou <hibou@13f79535-47bb-0310-9956-ffa450edef68>
2011-03-04 14:18:26 +0000

    Make a copy of Ivy's site into the new layout
    git-svn-id: http://svn.apache.org/repos/asf/ant@1077959 13f79535-47bb-0310-9956-ffa450edef68

---
+++ site/ivy/sources/config.js
@@ -0,0 +1,8 @@
+xooki.util.mix({debug:true, 
+	jira: {ids: ['IVY'], url: 'http://issues.apache.org/jira'}, 
+	shortcuts: {
+		svn: {pre: 'https://svn.apache.org/repos/asf/ant/ivy/core/trunk/'},
+		ant: {pre: xooki.c.relativeRoot+'history/latest-milestone/use/', post:'.html'},
+		doc: {pre: xooki.c.relativeRoot+'history/latest-milestone/', post:'.html'}
+	}
+}, xooki.c, false);
