2ec698827c0aabe220735550ae890e490383c4b7
hibou <hibou@13f79535-47bb-0310-9956-ffa450edef68>
2014-10-26 18:45:23 +0000

    add git source dir and file shortcuts for xooki
    git-svn-id: http://svn.apache.org/repos/asf/ant@1634378 13f79535-47bb-0310-9956-ffa450edef68

--- site/ivy/sources/config.js
+++ site/ivy/sources/config.js
@@ -1,6 +1,8 @@
 xooki.util.mix({debug:true, 
 	jira: {ids: ['IVY'], url: 'http://issues.apache.org/jira'}, 
 	shortcuts: {
+		gitdir: {pre: 'https://git-wip-us.apache.org/repos/asf?p=ant-ivy.git;a=tree;f='},
+		gitfile: {pre: 'https://git-wip-us.apache.org/repos/asf?p=ant-ivy.git;a=blob;f='},
 		ant: {pre: xooki.c.relativeRoot+'history/latest-milestone/use/', post:'.html'},
 		doc: {pre: xooki.c.relativeRoot+'history/latest-milestone/', post:'.html'}
 	}
